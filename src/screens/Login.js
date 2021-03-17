a8018e3550fdd7f0ed3cdf3a10849de0e91ad73e
MuhammadRafeh <muhammadrafehatique@gmail.com>
2021-03-17 14:21:22 +0300

    Added auth screens

---
+++ src/screens/Login.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+const Login = props => {
+    return (
+        <View>
+            <Text>
+                Login Page
+            </Text>
+        </View>
+    );
+}
+
+export default Login;
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff'
+    }
+});
---
+++ src/screens/Signup.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Signup = props => {
+    return (
+        <View>
+            <Text>
+                Sign Up Page
+            </Text>
+        </View>
+    );
+}
+
+export default Signup;
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff'
+    }
+});
