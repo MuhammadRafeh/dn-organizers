77f94345022d4da6797db4e89189394a26537051
MuhammadRafeh <muhammadrafehatique@gmail.com>
2021-03-18 09:10:58 +0300

    Loading fonts and images done

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import Login from './src/screens/Login';
 import { Ionicons } from '@expo/vector-icons';
-import { useFonts } from 'expo-font';
+// import { useFonts } from 'expo-font';
+import * as Font from 'expo-font';
+import { Asset } from 'expo-asset';
+import AppLoading from 'expo-app-loading';
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
 
 export default function App() {
-  const [loaded] = useFonts({
-    headings: require('./assets/fonts/main-headings.ttf'),
-    Montserrat: require('./assets/fonts/sub-headings.ttf'),
-    joining: require('./assets/fonts/stylish.ttf')
-  });
+  const [isReady, setIsReady] = useState(false);
 
-  if (!loaded) {
-    return <ActivityIndicator color={'blue'} size={50}/>;
+  const _loadAssetsAsync = async () => {
+    const imageAssets = cacheImages([
+      require('./assets/images/login-background.jpg')
+    ]);
+
+    await Promise.all([...imageAssets]);
+    await Font.loadAsync({
+      headings: require('./assets/fonts/main-headings.ttf'),
+      subHeadings: require('./assets/fonts/sub-headings.ttf'),
+      joining: require('./assets/fonts/stylish.ttf')
+    });
   }
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={_loadAssetsAsync}
+        onFinish={() => setIsReady(true)}
+        onError={console.warn}
+      />
+    );
+  }
+
+  // const [loaded] = useFonts({
+  // headings: require('./assets/fonts/main-headings.ttf'),
+  // Montserrat: require('./assets/fonts/sub-headings.ttf'),
+  // joining: require('./assets/fonts/stylish.ttf')
+  // });
+
+  // if (!loaded) {
+  //   return <ActivityIndicator color={'blue'} size={50}/>;
+  // }
 
   return (
     <PaperProvider
