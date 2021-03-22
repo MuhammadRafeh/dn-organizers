98abc9ccc4527e77668586a8fbe126a138e9309b
MuhammadRafeh <muhammadrafehatique@gmail.com>
2021-03-22 22:55:17 +0300

    Added Screen and slider to it

---
+++ src/screens/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Carousel from '../components/Carousel';
+import dummydata from '../data/Data';
+
+const Home = props => {
+    return (
+        <Carousel  />
+    );
+}
+
+export default Home;
+
+const styles = StyleSheet.create({
+
+});
