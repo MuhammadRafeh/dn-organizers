e5eaa3a1280943c2fd1e527e983d6bfc3f318d75
MuhammadRafeh <muhammadrafehatique@gmail.com>
2021-03-24 18:13:11 +0300

    Updated Home header

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Carousel from '../components/Carousel';
 import { dummyData } from '../data/Data';
 
 const Home = props => {
+    useEffect(() => {
+        props.navigation.setOptions({
+            headerShown: true,
+            headerTitle: "Home",
+            headerTintColor: 'white',
+            headerBackTitleStyle: {
+                color: 'white'
+            },
+            headerTitleStyle: { 
+                alignSelf: 'center'
+            },
+            headerStyle: {
+                backgroundColor: 'blue',
+            }
+        });
+    }, []);
     return (
         <Carousel data={dummyData} />
+
     );
 }
 
