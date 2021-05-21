68417fc50db6261606d7b2882fd1384cfcda563d
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2021-05-21 17:49:45 +0500

    Added screenshot library

--- src/screens/User/UserInvoices.js
+++ src/screens/User/UserInvoices.js
@@ -1,15 +1,23 @@
 // User Side Screen
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Text, StyleSheet, FlatList, Button, Image } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import Header from '../../components/Header';
 import firebase from 'firebase';
 import { setPendingInvoices } from '../../redux/actions';
+import PendingInvoices from '../../models/pendingInvoices';
+import Card from '../../components/Card';
+
+// import ViewShot from "react-native-view-shot";
 
 const UserInvoices = props => {
     const [pendingInvoices, email] = useSelector(state => [state.invoices.pendingInvoices, state.auth.email]);
     const dispatch = useDispatch();
     const [isRefreshing, setIsRefreshing] = useState(false);
+
+    const [imageUri, setImageUri] = useState('');
+
+    const screenShot = useRef(null);
 
     const pullData = () => {
         setIsRefreshing(true);
@@ -23,21 +31,55 @@ const UserInvoices = props => {
         pullData();
     }, [])
     // console.log(pendingInvoices)
+    // const onCapture = () => {
+    //     screenShot.current.capture().then(uri => {
+    //         console.log("do something with ", uri);
+    //         setImageUri(uri);
+    //     });
+    // }
     return (
         <View style={styles.screen}>
             <Header navigation={props.navigation} invoices />
             <FlatList
                 onRefresh={pullData}
                 refreshing={isRefreshing}
-                data={pendingInvoices}
+                data={pendingInvoices} //[new PendingInvoices(), .....]
                 renderItem={(item) => {
-                    return <View>
-                        <Text>
-                            {item.item.id}
-                        </Text>
-                    </View>
+                    return (
+                        <Card style={{ marginHorizontal: 10, marginBottom: 8, marginTop: 8, overflow: 'hidden' }}>
+                            <View style={styles.header}>
+                                {
+                                    item.item.isPackage ? (
+                                        <Text>
+                                            Package
+                                        </Text>) : (
+                                        <Text>
+                                            Custom
+                                        </Text>
+                                    )
+                                }
+                                <Text>
+
+                                </Text>
+                                <Text>
+                                    {item.item.serPackName}
+                                </Text>
+                            </View>
+                        </Card>
+                    )
                 }}
             />
+            {/* <ViewShot style={{backgroundColor: 'white'}} ref={screenShot} options={{ format: "jpg", quality: 0.9 }}>
+                <Text>...Something to rasterize...</Text>
+                <Text>...Something to rasterize...</Text>
+                <Text>...Something to rasterize...</Text>
+                <Text>...Something to rasterize...</Text>
+                <Text>...Something to rasterize...</Text>
+                <Text>...Something to rasterize...</Text>
+            </ViewShot> */}
+            {/* <h1>asd</h1> */}
+            {/* <Button title={'preess'} onPress={onCapture} /> */}
+            {/* <Image source={{uri: imageUri}} style={{width: '100%', height: 400}} resizeMode={'contain'}/> */}
         </View>
     );
 }
@@ -49,5 +91,10 @@ const styles = StyleSheet.create({
         flex: 1,
         // justifyContent: 'center',
         // alignItems: 'center'
+    },
+    header: {
+        backgroundColor: '#add8e6',
+        flexDirection: 'row',
+        justifyContent: 'space-around'
     }
 });
